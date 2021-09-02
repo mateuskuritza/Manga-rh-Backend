4aac406a13714db40b2a7890d5735974dbad8dbc
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-09-02 14:13:19 -0300

    get collaborators and collaborator by name

---
+++ src/collaborators/collaborators.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { CollaboratorsService } from './collaborators.service';
+
+@Controller()
+export class CollaboratorsController {
+  constructor(private readonly collaboratorService: CollaboratorsService) {}
+
+  @Get('collaborator')
+  findAll() {
+    return this.collaboratorService.findAll();
+  }
+
+  @Get('collaborator/:name')
+  findOne(@Param() name: string) {
+    return this.collaboratorService.findOne(name);
+  }
+}
---
+++ src/collaborators/entities/collaborator.entity.ts
@@ -0,0 +1,47 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('collaborators')
+export class Collaborator {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    unique: true,
+  })
+  name: string;
+
+  @Column({
+    unique: true,
+  })
+  cpf: string;
+
+  @Column()
+  email: string;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: true, default: null })
+  valid: boolean;
+
+  @UpdateDateColumn()
+  update_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @BeforeInsert()
+  generatedId() {
+    if (this.id) return;
+    this.id = uuidv4();
+  }
+}
