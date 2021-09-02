e3cc3d47f6ea2c56fc3012be9db4ff1dd557fb22
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-09-02 13:13:34 -0300

    port env variable

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 4000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
@@ -11,6 +13,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap();
